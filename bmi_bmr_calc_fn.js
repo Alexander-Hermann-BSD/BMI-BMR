34e679836fc58e63e689c0e735f7b3048dee6b92
Heinrich Alexandra Hermann <a.hermann@hermann-bsd.de>
2021-02-13 03:37:58 +0100

    added short gel info

--- bmi_bmr_calc_fn.js
+++ bmi_bmr_calc_fn.js
@@ -4,6 +4,31 @@
  *
  *  written by A.Hermann 
  *  this software is OpenSourc and is published unter the GPL
+ 
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    Alexandra Hermann – Beratung, Software, Design 
+    Copyright (C) 2013 - 2021 Alexandra Hermann
+    Alexandra Hermann – Beratung, Software, Design comes with ABSOLUTELY NO 
+    WARRANTY; for details type `show w'. This is free software, and you are 
+    welcome to redistribute it under certain conditions; type `show c' for details.
+
+Alexandra Hermann – Beratung, Software, Design, hereby disclaims all copyright 
+interest in the program (which makes passes at compilers) written by 
+Heinrich Alexandra Hermann.
+
  */
 
 /**
